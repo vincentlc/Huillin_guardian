d33b1dbfc3b7b29b9cbe8a8d70e98aa799c3f857
vincentlc <vincent.lc@laposte.net>
2025-01-24 17:20:48 -0300

    update of the python file to avoid error, add try catch and log

--- Comunication_huillin/read_mqtt_send_lora.py
+++ Comunication_huillin/read_mqtt_send_lora.py
@@ -1,12 +1,12 @@
-# Author: Renzo Mischianti
-# Website: www.mischianti.org
-#
+# Author: Vincent le costaouec
+# Code inspired in work of Renzo Mischianti (www.mischianti.org)
+
 # Description:
-# This script demonstrates how to use the E22 LoRa module with RaspberryPi.
-# Sending dictionary
+# This script read messages from the node-red that send it on a MQTT channel
+# And send the data read to lora 
 #
-# Note: This code was written and tested using RaspberryPi on an ESP32 board.
-#       It works with other boards, but you may need to change the UART pins.
+# Note: This code was written and tested using RaspberryPi.
+#       when use double check that you are using the correct UART and pins.
 
 import serial
 import time
@@ -16,12 +16,29 @@ from lora_e22_constants import RssiAmbientNoiseEnable, RssiEnableByte
 from lora_e22_operation_constant import ResponseStatusCode
 
 import paho.mqtt.client as mqtt
+import logging
+import RPi.GPIO as GPIO
+
+file="huillin_read_mqtt_send_lora"
+#innit logger
+log = logging.getLogger(file)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    filename=file+".log",
+    encoding="utf-8",
+    filemode="a"
+)
+
+GPIO.setwarnings(False) #de activate the GPIO warning to avoir the waring of re-use of port
 
 # Initialize the LoRaE22 module
-loraSerial = serial.Serial('/dev/ttyUSB1') #, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+loraSerial = serial.Serial('/dev/ttyS0') #, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 lora = LoRaE22('400T22D', loraSerial, m0_pin=23, m1_pin=24)
 code = lora.begin()
-print("Initialization: {}", ResponseStatusCode.get_description(code))
+log.info("Initialization: {}"+ResponseStatusCode.get_description(code))
+
+client_ID = "400T22D" #id of the mqtt
 
 # Set the configuration to default values and print the updated configuration to the console
 # Not needed if already configured
@@ -29,28 +46,31 @@ configuration_to_set = Configuration('400T22D')
 # To enable RSSI, you must also enable RSSI on receiver
 configuration_to_set.TRANSMISSION_MODE.enableRSSI = RssiEnableByte.RSSI_ENABLED
 code, confSetted = lora.set_configuration(configuration_to_set)
-print("Set configuration: {}", ResponseStatusCode.get_description(code))
+log.info("Set configuration: {}"+str(ResponseStatusCode.get_description(code)))
 
-def on_connect(client, userdata, flags, rc):
-  print("Connected with result code " + str(rc))
-  client.subscribe("huillin/data")
+def on_connect(client, userdata, flags, rc, properties):
+  log.info("Connected with result code " + str(rc))
+  if rc == 0 : #mean success
+    client.subscribe("huillin/data")
   
 def on_message(client, userdata, msg):
-  print(msg.topic + " " + str(msg.payload))
+  log.info(msg.topic + " " + str(msg.payload))
+  log.info(len(msg.payload.decode('utf-8')))                                                              
   code = lora.send_transparent_dict(msg.payload.decode('utf-8'))
-  print("Send message: {}", ResponseStatusCode.get_description(code))
+  log.info("Send message: {}"+ResponseStatusCode.get_description(code))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("10.3.141.1", 1883, 60)
+try:
+    # connect client to MQTT
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("10.3.141.1", 1883, 60)
 
-client.loop_forever()
-
-
-# Send a dictionary message (transparent)
-#data = {'key1': 'value1', 'key2': 'value2'}
-#while 1:
-#     code = lora.send_transparent_dict(data)
-#     print("Send message: {}", ResponseStatusCode.get_description(code))
-#     time.sleep(5)
+    client.loop_forever()
+    
+except KeyboardInterrupt:
+    # Manejar interrupción de teclado
+    log.info("Interrupción de teclado detectada. Cerrando conexión.")
+    client.disconnect()
+except Exception as e: 
+    log.error("hubo un error: "+str(e))
