17de903725748bf835a5faa14f48e281100e13c8
vincentlc <vincent.lc@laposte.net>
2025-02-10 15:43:14 -0300

    bug fix and add Q&A

--- Comunication_estacion/start_lora_broker.sh
+++ Comunication_estacion/start_lora_broker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 user="estacion" # if user on the board is different please change the line
-cd /home/$user_install/lora_project/ 
+cd /home/$user/lora_project/ 
 source .venv/bin/activate
 python3 read_lora_send_mqtt.py
