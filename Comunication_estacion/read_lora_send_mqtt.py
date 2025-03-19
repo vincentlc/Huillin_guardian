0899dfa40fa6ee51a97c78dc33fc49bd5d9bb0b1
vincentlc <vincent.lc@laposte.net>
2025-03-19 16:58:31 -0300

    improve file read lora

--- Comunication_estacion/read_lora_send_mqtt.py
+++ Comunication_estacion/read_lora_send_mqtt.py
@@ -18,29 +18,23 @@ import paho.mqtt.client as mqtt
 import logging
 import RPi.GPIO as GPIO
 
-file="estacion_read_mqtt_send_lora"
-#innit logger
+# Constants
+MQTT_BROKER = "10.3.141.1"
+MQTT_TOPIC = 'huillin/data'
+SLEEP_TIME = 2  # Configurable sleep time in seconds
+
+# Initialize logger
+file = "estacion_read_mqtt_send_lora"
 log = logging.getLogger(file)
-logging.basicConfig( #logging into a local file
+logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    #level=logging.INFO,
     level=logging.DEBUG,
-    #filename=file+".log",
+    #filename=file + ".log",
     encoding="utf-8",
     #filemode="a"
 )
 
 GPIO.setwarnings(False) #de activate the GPIO warning to avoir the waring of re-use of port
-
-# Configuración del servidor MQTT
-MQTT_BROKER = "10.3.141.1" #"127.0.0.1" # Direccion de broker MQTT
-MQTT_TOPIC = 'huillin/data'  # Tema MQTT utilizado
-
-# Initialize the LoRaE22 module
-loraSerial = serial.Serial('/dev/ttyUSB0') #, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-lora = LoRaE22('400T22D', loraSerial, m0_pin=23, m1_pin=24)
-code = lora.begin()
-log.info("Initialization:"+ ResponseStatusCode.get_description(code))
 
 # I found out that if i config it everytime it was creating conflict, but i leave it in case needed.
 # Set the configuration to default values and print the updated configuration to the console
@@ -52,36 +46,62 @@ log.info("Initialization:"+ ResponseStatusCode.get_description(code))
 #code, confSetted = lora.set_configuration(configuration_to_set)
 #log.info("Set configuration: "+ ResponseStatusCode.get_description(code))
 
-# Callback para conexión exitosa al broker MQTT
+
 def on_connect(client, userdata, flags, rc):
+    """Callback for successful MQTT connection."""
     log.info("Conectado al broker MQTT con resultado: " + str(mqtt.connack_string(rc)))
 
-try:
-    # Crear instancia del cliente MQTT
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-    client.on_connect = on_connect
+def main() -> None:
+    """Main function to read LoRa messages and send them to MQTT."""
+    
+    try:
+        # Initialize the LoRaE22 module
+        lora_serial = serial.Serial('/dev/ttyUSB0') #, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+        lora = LoRaE22('400T22D', lora_serial, m0_pin=23, m1_pin=24)
+        code = lora.begin()
+        log.info("Initialization:"+ ResponseStatusCode.get_description(code))
 
-    # Conectar al broker MQTT
-    client.connect(MQTT_BROKER, 1883, 60)
+        # Crear instancia del cliente MQTT
+        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        client.on_connect = on_connect
+        # Conectar al broker MQTT
+        client.connect(MQTT_BROKER, 1883, 60)
 
-    log.info("Waiting for messages...")
-    while True:
-        if lora.available() > 0: #wait for lora to be available
-            try:
-                code, value, rssi = lora.receive_dict(rssi=True) # reading the data
-                
-                # print debug message
-                log.debug('RSSI: '+ str(rssi)) 
-                log.debug(str(ResponseStatusCode.get_description(code)))
+        log.info("Waiting for messages...")
 
-                client.publish(MQTT_TOPIC, str(value)) #publish value to mqtt
-                log.debug(value)
-                time.sleep(2) #sleep time required in order to not overload the lora module
-            except UnicodeDecodeError as e:
-                log.error("UnicodeDecodeError "+str(e))
-            except Exception as e: 
-                log.error("hubo un error: "+str(e))
-except KeyboardInterrupt:
-    # Manejar interrupción de teclado
-    log.info("Interrupción de teclado detectada. Cerrando conexión.")
-    client.disconnect()
+        while True:
+            if lora.available() > 0: #wait for lora to be available
+                try:
+                    code, value, rssi = lora.receive_dict(rssi=True) # reading the data
+                    
+                    # Log RSSI and status code
+                    log.debug(f"RSSI: {rssi},Status: {ResponseStatusCode.get_description(code)} ")
+
+                    # Publish to MQTT
+                    if isinstance(value, dict) or isinstance(value, str):
+                        client.publish(MQTT_TOPIC, str(value))
+                        log.debug(f"Published to MQTT: {value}")
+                    else:
+                        log.warning("Invalid payload type received from LoRa")
+                    
+                    time.sleep(SLEEP_TIME) #sleep time required in order to not overload the lora module
+                except UnicodeDecodeError as e:
+                    log.error(f"UnicodeDecodeError: {e}")
+                except Exception as e: 
+                    log.error(f"An error occurred: {e}")
+    except KeyboardInterrupt:
+        # Manejar interrupción de teclado
+        log.info("Interrupción de teclado detectada. Cerrando conexión.")
+        client.disconnect()
+    except Exception as e:
+        log.error(f"Critical error: {e}")
+    finally:
+        # Cleanup
+        if 'lora_serial' in locals() and lora_serial.is_open:
+            lora_serial.close()
+            log.info("LoRa serial connection closed.")
+        client.disconnect()
+        log.info("MQTT client disconnected.")
+
+if __name__ == "__main__":
+    main()
